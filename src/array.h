3191cfe9f7462918563dcf4767af116753b387f5
Sascha Peilicke <sasch.pe@gmx.de>
2010-05-04 17:32:07 +0200

    Added more Boost-like license header. Added partial template specialization for arrays with zero size.

--- src/array.h
+++ src/array.h
@@ -1,12 +1,26 @@
-
-// Copyright Sascha Peilicke <sasch.pe@gmx.de> 2010.
-//
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
-
-#ifndef ROBUST_ARRAY_H
-#define ROBUST_ARRAY_H
+/* The following code declares class array,
+ * an robust STL container (as wrapper) for arrays of constant size with
+ * checksumming. This allows to monitor the validity of the array's content.
+ *
+ * This class is based on boost::array by Nicolai M. Josuttis.
+ * See
+ *      http://www.boost.org/libs/array/
+ * for documentation.
+ *
+ * The original author site is at: http://saschpe.wordpress.com/
+ *
+ * (C) Copyright Sascha Peilicke 2010.
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * 20 April 2010 - Initial Revision (Sascha Peilicke)
+ *
+ * May 4th, 2010
+ */
+#ifndef BOOST_ROBUST_ARRAY_HPP
+#define BOOST_ROBUST_ARRAY_HPP
 
 #include <boost/detail/workaround.hpp>
 
@@ -31,200 +45,278 @@
 // FIXES for broken compilers
 #include <boost/config.hpp>
 
+
 namespace robust {
 
-template <class T, std::size_t N = 64>
-class array
-{
-public:
-    // type definitions
-    typedef T              value_type;
-    typedef T*             iterator;
-    typedef const T*       const_iterator;
-    typedef T&             reference;
-    typedef const T&       const_reference;
-    typedef std::size_t    size_type;
-    typedef std::ptrdiff_t difference_type;
-
-    array(const T &value = 0, void *head = NULL, void *tail = NULL)
-        : m_head(head), m_tail(tail)
+    /**
+     * Checksummed array
+     */
+    template <class T, std::size_t N>
+    class array
     {
-        for (size_type i = 0; i < N; i++) {
-            m_elements[i] = value;
+    public:
+        // type definitions
+        typedef T              value_type;
+        //TODO: replace 'iterator' with safe class
+        typedef T*             iterator;
+        typedef const T*       const_iterator;
+        //TODO: replace 'reference' with safe class
+        typedef T&             reference;
+        typedef const T&       const_reference;
+        typedef std::size_t    size_type;
+        typedef std::ptrdiff_t difference_type;
+
+        // contructor
+        array(const T &value = 0) {
+            fill(value);
         }
 
-        // compute and store initial CRC checksums
-        boost::crc_32_type crc;
-        crc.process_bytes(&m_elements, N * sizeof(T));
-        m_crc1 = crc.checksum();
-        m_crc2 = crc.checksum();
-    }
+        // iterator support
+        iterator begin() { return m_elements; }
+        const_iterator begin() const { return m_elements; }
+        iterator end() { return m_elements + N; }
+        const_iterator end() const { return m_elements + N; }
 
-    // iterator support
-    iterator begin() { return m_elements; }
-    const_iterator begin() const { return m_elements; }
-    iterator end() { return m_elements + N; }
-    const_iterator end() const { return m_elements + N; }
-
-    // reverse iterator support
+        // reverse iterator support
 #if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) && !defined(BOOST_MSVC_STD_ITERATOR) && !defined(BOOST_NO_STD_ITERATOR_TRAITS)
-    typedef std::reverse_iterator<iterator> reverse_iterator;
-    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+        typedef std::reverse_iterator<iterator> reverse_iterator;
+        typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+#elif defined(_MSC_VER) && (_MSC_VER == 1300) && defined(BOOST_DINKUMWARE_STDLIB) && (BOOST_DINKUMWARE_STDLIB == 310)
+        // workaround for broken reverse_iterator in VC7
+        typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, iterator,
+                                                  reference, iterator, reference> > reverse_iterator;
+        typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, const_iterator,
+                                                  const_reference, iterator, reference> > const_reverse_iterator;
+#elif defined(_RWSTD_NO_CLASS_PARTIAL_SPEC)
+        typedef std::reverse_iterator<iterator, std::random_access_iterator_tag,
+                                      value_type, reference, iterator, difference_type> reverse_iterator;
+        typedef std::reverse_iterator<const_iterator, std::random_access_iterator_tag,
+                                      value_type, const_reference, const_iterator, difference_type> const_reverse_iterator;
+#else
+        // workaround for broken reverse_iterator implementations
+        typedef std::reverse_iterator<iterator,T> reverse_iterator;
+        typedef std::reverse_iterator<const_iterator,T> const_reverse_iterator;
+#endif
+        reverse_iterator rbegin() { return reverse_iterator(end()); }
+        const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+        reverse_iterator rend() { return reverse_iterator(begin()); }
+        const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+
+        // operator[] with range check
+        reference operator[](size_type i) { rangecheck(i); return m_elements[i]; }
+        const_reference operator[](size_type i) const { rangecheck(i); return m_elements[i]; }
+
+        // at() with range check
+        reference at(size_type i) { rangecheck(i); return m_elements[i]; }
+        const_reference at(size_type i) const { rangecheck(i); return m_elements[i]; }
+
+        // front() and back()
+        reference front() { return m_elements[0]; }
+        const_reference front() const { return m_elements[0]; }
+        reference back() { return m_elements[N - 1]; }
+        const_reference back() const { return m_elements[N - 1]; }
+
+        // size is constant
+        static size_type size() { return N; }
+        static bool empty() { return false; }
+        static size_type max_size() { return N; }
+        enum { static_size = N };
+
+        // swap (note: linear complexity)
+        void swap (array<T, N>& y) {
+            for (size_type i = 0; i < N; ++i) {
+                boost::swap(m_elements[i], y.m_elements[i]);
+            }
+            boost::swap(m_crc1, y.m_crc1);
+            boost::swap(m_crc2, y.m_crc2);
+        }
+
+        //NOTE: those methods would bypass checksumming
+        /*// direct access to data (read-only)
+        const T* data() const { return m_elements; }
+        T* data() { return m_elements; }
+
+        // use array as C array (direct read/write access to data)
+        T* c_array() { return m_elements; }*/
+
+        // assignment with type conversion
+        template <typename T2>
+        array<T, N>& operator= (const array<T2, N>& rhs) {
+            std::copy(rhs.begin(), rhs.end(), begin());
+            update_checksums();
+            return *this;
+        }
+
+        // assign one value to all elements
+        void assign (const T& value) { fill(value); }    // A synonym for fill
+        void fill (const T& value) {
+            std::fill_n(begin(), size(),value);
+            update_checksums();
+        }
+
+        // check range (may be private because it is static)
+        static void rangecheck (size_type i) {
+            if (i >= size()) {
+                std::out_of_range e("array<>: index out of range");
+                boost::throw_exception(e);
+            }
+        }
+
+        bool is_valid() {
+            bool valid = true;
+            //valid &= check_checksums();
+            //TODO: check crcs
+            return valid;
+        }
+
+    private:
+        void update_checksums() {
+            // compute and store CRC checksums
+            boost::crc_32_type crc;
+            crc.process_bytes(&m_elements, N * sizeof(T));
+            m_crc1 = crc.checksum();
+            m_crc2 = m_crc1;
+        }
+
+        unsigned int m_crc1;            // first checksum
+        T m_elements[N];                // fixed-size array of elements of type T
+        unsigned int m_crc2;            // fecond checksum
+    };
+
+#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+    template<class T>
+    class array<T, 0>
+    {
+    public:
+        // type definitions
+        typedef T              value_type;
+        typedef T*             iterator;
+        typedef const T*       const_iterator;
+        typedef T&             reference;
+        typedef const T&       const_reference;
+        typedef std::size_t    size_type;
+        typedef std::ptrdiff_t difference_type;
+
+        // iterator support
+        iterator begin() { return iterator(reinterpret_cast<T *>(this)); }
+        const_iterator begin() const { return const_iterator(reinterpret_cast<const T *>(this)); }
+        iterator end() { return begin(); }
+        const_iterator end() const { return begin(); }
+
+        // reverse iterator support
+#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) && !defined(BOOST_MSVC_STD_ITERATOR) && !defined(BOOST_NO_STD_ITERATOR_TRAITS)
+        typedef std::reverse_iterator<iterator> reverse_iterator;
+        typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 #elif defined(_MSC_VER) && (_MSC_VER == 1300) && defined(BOOST_DINKUMWARE_STDLIB) && (BOOST_DINKUMWARE_STDLIB == 310)
     // workaround for broken reverse_iterator in VC7
-    typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, iterator,
-                                              reference, iterator, reference> > reverse_iterator;
-    typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, const_iterator,
-                                              const_reference, iterator, reference> > const_reverse_iterator;
+        typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, iterator,
+                                                  reference, iterator, reference> > reverse_iterator;
+        typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, const_iterator,
+                                                  const_reference, iterator, reference> > const_reverse_iterator;
 #elif defined(_RWSTD_NO_CLASS_PARTIAL_SPEC)
-    typedef std::reverse_iterator<iterator, std::random_access_iterator_tag,
-                                  value_type, reference, iterator, difference_type> reverse_iterator;
-    typedef std::reverse_iterator<const_iterator, std::random_access_iterator_tag,
-                                  value_type, const_reference, const_iterator, difference_type> const_reverse_iterator;
+        typedef std::reverse_iterator<iterator, std::random_access_iterator_tag,
+                                      value_type, reference, iterator, difference_type> reverse_iterator;
+        typedef std::reverse_iterator<const_iterator, std::random_access_iterator_tag,
+                                      value_type, const_reference, const_iterator, difference_type> const_reverse_iterator;
 #else
-    // workaround for broken reverse_iterator implementations
-    typedef std::reverse_iterator<iterator,T> reverse_iterator;
-    typedef std::reverse_iterator<const_iterator,T> const_reverse_iterator;
+        // workaround for broken reverse_iterator implementations
+        typedef std::reverse_iterator<iterator, T> reverse_iterator;
+        typedef std::reverse_iterator<const_iterator, T> const_reverse_iterator;
 #endif
-    reverse_iterator rbegin() { return reverse_iterator(end()); }
-    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
-    reverse_iterator rend() { return reverse_iterator(begin()); }
-    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
 
-    // operator[]
-    reference operator[](size_type i) {
-        //TODO: assert( i < N && "out of range" );
-        return m_elements[i];
-    }
-    const_reference operator[](size_type i) const {
-        //TODO: assert( i < N && "out of range" );
-        return m_elements[i];
-    }
+        reverse_iterator rbegin() { return reverse_iterator(end()); }
+        const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+        reverse_iterator rend() { return reverse_iterator(begin()); }
+        const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
 
-    // at() with range check
-    reference at(size_type i) { rangecheck(i); return m_elements[i]; }
-    const_reference at(size_type i) const { rangecheck(i); return m_elements[i]; }
+        // operator[]
+        reference operator[](size_type /*i*/) { return failed_rangecheck(); }
+        const_reference operator[](size_type /*i*/) const { return failed_rangecheck(); }
 
-    // front() and back()
-    reference front() { return m_elements[0]; }
-    const_reference front() const { return m_elements[0]; }
-    reference back() { return m_elements[N - 1]; }
-    const_reference back() const { return m_elements[N - 1]; }
+        // at() with range check
+        reference at(size_type /*i*/) { return failed_rangecheck(); }
+        const_reference at(size_type /*i*/) const { return failed_rangecheck(); }
 
-    // size is constant
-    static size_type size() { return N; }
-    static bool empty() { return false; }
-    static size_type max_size() { return N; }
-    enum { static_size = N };
+        // front() and back()
+        reference front() { return failed_rangecheck(); }
+        const_reference front() const { return failed_rangecheck(); }
 
-    // swap (note: linear complexity)
-    void swap (array<T, N>& y) {
-        for (size_type i = 0; i < N; ++i) {
-            boost::swap(m_elements[i], y.m_elements[i]);
-        }
-        boost::swap(m_crc1, y.m_crc1);
-        boost::swap(m_crc2, y.m_crc2);
-    }
+        reference back() { return failed_rangecheck(); }
+        const_reference back() const { return failed_rangecheck(); }
 
-    //NOTE: those methods would bypass checksumming
-    /*// direct access to data (read-only)
-    const T* data() const { return m_elements; }
-    T* data() { return m_elements; }
+        // size is constant
+        static size_type size() { return 0; }
+        static bool empty() { return true; }
+        static size_type max_size() { return 0; }
+        enum { static_size = 0 };
 
-    // use array as C array (direct read/write access to data)
-    T* c_array() { return m_elements; }*/
+        void swap (array<T, 0>& /*y*/) {}
 
-    // assignment with type conversion
-    template <typename T2>
-    array<T, N>& operator= (const array<T2, N>& rhs) {
-        std::copy(rhs.begin(), rhs.end(), begin());
-        return *this;
-    }
+        // direct access to data (read-only)
+        const T* data() const { return 0; }
+        T* data() { return 0; }
 
-    // assign one value to all elements
-    void assign (const T& value) { fill(value); }    // A synonym for fill
-    void fill   (const T& value) { std::fill_n(begin(), size(),value); }
+        // use array as C array (direct read/write access to data)
+        T* c_array() { return 0; }
 
-    // check range (may be private because it is static)
-    static void rangecheck (size_type i) {
-        if (i >= size()) {
-            std::out_of_range e("array<>: index out of range");
+        // assignment with type conversion
+        template <typename T2>
+        array<T, 0>& operator= (const array<T2, 0>& ) { return *this; }
+
+        // assign one value to all elements
+        void assign (const T& value) { fill(value); }
+        void fill (const T& /*value*/) {}
+
+        // check range (may be private because it is static)
+        static reference failed_rangecheck () {
+            std::out_of_range e("attempt to access element of an empty array");
             boost::throw_exception(e);
+#if defined(BOOST_NO_EXCEPTIONS) || !defined(BOOST_MSVC)
+            //
+            // We need to return something here to keep
+            // some compilers happy: however we will never
+            // actually get here....
+            //
+            static T placeholder;
+            return placeholder;
+#endif
         }
+    };
+#endif
+
+    // comparisons
+    template<class T, std::size_t N>
+    bool operator== (const array<T, N>& x, const array<T, N>& y) {
+        return std::equal(x.begin(), x.end(), y.begin());
+    }
+    template<class T, std::size_t N>
+    bool operator< (const array<T, N>& x, const array<T, N>& y) {
+        return std::lexicographical_compare(x.begin(), x.end(), y.begin(), y.end());
+    }
+    template<class T, std::size_t N>
+    bool operator!= (const array<T, N>& x, const array<T, N>& y) {
+        return !(x == y);
+    }
+    template<class T, std::size_t N>
+    bool operator> (const array<T, N>& x, const array<T, N>& y) {
+        return y < x;
+    }
+    template<class T, std::size_t N>
+    bool operator<= (const array<T, N>& x, const array<T, N>& y) {
+        return !(y < x);
+    }
+    template<class T, std::size_t N>
+    bool operator>= (const array<T, N>& x, const array<T, N>& y) {
+        return !(x < y);
     }
 
-    bool is_valid() {
-        bool valid = true;
-        valid &= check_head_pointer();
-        valid &= check_tail_pointer();
-        valid &= check_checksums();
-        return valid;
+    // global swap()
+    template<class T, std::size_t N>
+    inline void swap (array<T, N>& x, array<T, N>& y) {
+        x.swap(y);
     }
 
-private:
-    bool check_head_pointer() {
-        bool success = true;
-        // check if the circular reference on head is ok
-        if (m_head != NULL) {
-            //**m_head == &m_head;
-        }
-    }
-
-    bool check_tail_pointer() {
-        bool success = true;
-        // check if the circular reference on tail is ok
-        if (m_tail != NULL) {
-            //success &= **m_tail == &m_tail;
-        }
-        return success;
-    }
-
-    unsigned int check_checksums() {
-        //TODO: check crcs
-        return m_crc1;
-    }
-
-    void *m_head;
-    unsigned int m_crc1;            // first CRC-32 checksum
-    T m_elements[N];                // fixed-size array of elements of type T
-    unsigned int m_crc2;            // fecond CRC-32 checksum
-    void *m_tail;
-};
-
-
-// comparisons
-template<class T, std::size_t N>
-bool operator== (const array<T, N>& x, const array<T, N>& y) {
-    return std::equal(x.begin(), x.end(), y.begin());
-}
-template<class T, std::size_t N>
-bool operator< (const array<T, N>& x, const array<T, N>& y) {
-    return std::lexicographical_compare(x.begin(), x.end(), y.begin(), y.end());
-}
-template<class T, std::size_t N>
-bool operator!= (const array<T, N>& x, const array<T, N>& y) {
-    return !(x == y);
-}
-template<class T, std::size_t N>
-bool operator> (const array<T, N>& x, const array<T, N>& y) {
-    return y < x;
-}
-template<class T, std::size_t N>
-bool operator<= (const array<T, N>& x, const array<T, N>& y) {
-    return !(y < x);
-}
-template<class T, std::size_t N>
-bool operator>= (const array<T, N>& x, const array<T, N>& y) {
-    return !(x < y);
-}
-
-// global swap()
-template<class T, std::size_t N>
-inline void swap (array<T, N>& x, array<T, N>& y) {
-    x.swap(y);
-}
-
-} // namespace robust
+} /* namespace robust */
 
 template <class T, std::size_t N>
 std::ostream &operator<<(std::ostream &os, const robust::array<T, N> &array)
@@ -237,8 +329,9 @@ std::ostream &operator<<(std::ostream &os, const robust::array<T, N> &array)
     return os;
 }
 
+
 #if BOOST_WORKAROUND(BOOST_MSVC, >= 1400)
 # pragma warning(pop)
 #endif
 
-#endif // ROBUST_ARRAY_H
+#endif /* BOOST_ROBUST_ARRAY_HPP */
