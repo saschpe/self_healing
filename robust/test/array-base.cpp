e34eccef6245da9493eac2316f86d34a48c6b9cf
Sascha Peilicke <sasch.pe@gmx.de>
2010-05-04 22:18:31 +0200

    Expanded array-base unit test.

--- robust/test/array-base.cpp
+++ robust/test/array-base.cpp
@@ -20,20 +20,65 @@
 
 int main()
 {
-    robust::array<int, 64> a1;
+    robust::array<int, 16> a1;
     std::cout << "array 1: " << a1 << std::endl;
 
     assert(a1.at(0) == 0);
     assert(a1.at(a1.size() - 1) == 0);
 
+    int i = 3;
+    robust::array<int, 1>::reference r(i);
+
+    // testing class robust::array<>::reference assignment
     a1.front() = 3;
     a1.back() = 4;
     a1[2] = 4;
     a1.at(3) = 5;
 
-    robust::array<int, 1> a2(42);
-    std::cout << "array 2: " << a2 << std::endl;
-    assert(a2.at(0) == 42);
+    // testing class robust::array<>::reference comparison
+    assert(a1.front() == 3);
+    assert(a1.front() != 5);
+    assert(a1.front() >= 2);
+    std::cout << (a1.front() <= 2) << std::endl;
+    std::cout << (a1.front() > 3) << std::endl;
+    std::cout << (a1.front() < 3) << std::endl;
+
+    // testing class robust::array<>::reference arithmetic operations
+    std::cout << a1.front() + 2 << std::endl;
+    std::cout << a1.front() - 2 << std::endl;
+    std::cout << a1.front() * 2 << std::endl;
+    std::cout << a1.front() / 2 << std::endl;
+    std::cout << a1.front() % 2 << std::endl;
+
+    std::cout << a1[0];
+    /*std::cout << "a1[6] += 1: " << (a1[6] += 1) << " and a1[6]: " << a1[6].value() << std::endl;
+    std::cout << (a1[7] -= 1) << std::endl;
+    std::cout << (a1[7] *= 1) << std::endl;
+    std::cout << (a1[7] /= 1) << std::endl;
+    std::cout << (a1[7] %= 1) << std::endl;*/
+
+    // testing class robust::array<>::reference increment and decrement operations
+    a1[8]++;
+    ++a1[9];
+    a1[10]--;
+    --a1[11];
+
+    // testing iterators
+    std::cout << "array 1:  ";
+    std::copy(a1.begin(), a1.end(), std::ostream_iterator<int>(std::cout, ","));
+    std::cout << std::endl;
+
+    for (robust::array<int, 16>::iterator it = a1.begin(); it != a1.end(); it++) {
+        *it += 1;
+    }
+
+    std::cout << "array 1:  ";
+    for (robust::array<int, 16>::const_reverse_iterator rit = a1.rbegin(); rit != a1.rend(); rit++) {
+        std:: cout << *rit << ",";
+    }
+    std::cout << std::endl;
+
+    std::cout << "array 1: " << a1 << std::endl;
 
     return 0;
 }
