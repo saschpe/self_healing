8ec2bfcd30495cb3773bfc5cae2e0998d7ceec54
Sascha Peilicke <sasch.pe@gmx.de>
2010-05-05 12:25:35 +0200

    Refactored robust::array<>::reference into own generic class robust::reference<>. Also added robust::functor for functor objects used by robust::reference<> and others. Added corresponding header files to CMakeLists.txt

---
+++ robust/functor.hpp
@@ -0,0 +1,44 @@
+/* The following code declares class functor.
+ *
+ * The original author site is at: http://saschpe.wordpress.com/
+ *
+ * (C) Copyright Sascha Peilicke 2010.
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * 5 May 2010 - Initial Revision (Sascha Peilicke)
+ *
+ * May 5th, 2010
+ */
+#ifndef BOOST_ROBUST_FUNCTOR_HPP
+#define BOOST_ROBUST_FUNCTOR_HPP
+
+// FIXES for broken compilers
+#include <boost/config.hpp>
+
+
+namespace robust {
+
+    /**
+     * Abstract functor base class.
+     */
+    class functor
+    {
+    public:
+        virtual void operator()() = 0;
+    };
+
+    /**
+     * Empty default functor instance suitable as a default value.
+     */
+    static class void_functor : public functor {
+    public:
+        void operator()() {}
+    } void_functor;
+
+} // namespace robust
+
+
+#endif /* BOOST_ROBUST_FUNCTOR_HPP */
---
+++ robust/reference.hpp
@@ -0,0 +1,78 @@
+/* The following code declares class reference.
+ *
+ * The original author site is at: http://saschpe.wordpress.com/
+ *
+ * (C) Copyright Sascha Peilicke 2010.
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * 5 May 2010 - Initial Revision (Sascha Peilicke)
+ *
+ * May 5th, 2010
+ */
+#ifndef BOOST_ROBUST_REFERENCE_HPP
+#define BOOST_ROBUST_REFERENCE_HPP
+
+#include <ostream>
+
+// FIXES for broken compilers
+#include <boost/config.hpp>
+
+#include <robust/functor.hpp>
+
+
+namespace robust {
+
+    /**
+     * Reference to a value which optionally takes a functor that is called
+     * every time the value changes.
+     */
+    template <class T>
+    class reference
+    {
+    public:
+        reference(T& value, robust::functor &functor = void_functor)
+            : m_value(value), m_functor(functor) {}
+
+        reference& operator=(const T& rhs) { m_value = rhs; m_functor(); return *this; }
+        reference& operator=(const reference &rhs) { m_value = rhs.m_value; m_functor(); return *this; }
+
+        T operator+(const T& rhs) const { return m_value + rhs; }
+        T operator-(const T& rhs) const { return m_value - rhs; }
+        T operator*(const T& rhs) const { return m_value * rhs; }
+        T operator/(const T& rhs) const { return m_value / rhs; }
+        T operator%(const T& rhs) const { return m_value % rhs; }
+
+        reference& operator+=(const T& rhs) { m_value += rhs; m_functor(); return *this; }
+        reference& operator-=(const T& rhs) { m_value -= rhs; m_functor(); return *this; }
+        reference& operator*=(const T& rhs) { m_value *= rhs; m_functor(); return *this; }
+        reference& operator/=(const T& rhs) { m_value /= rhs; m_functor(); return *this; }
+        reference& operator%=(const T& rhs) { m_value %= rhs; m_functor(); return *this; }
+
+        reference& operator++() { ++m_value; m_functor(); return *this; }
+        reference& operator++(int) { m_value++; m_functor(); return *this; }
+        reference& operator--() { --m_value; m_functor(); return *this; }
+        reference& operator--(int) { m_value--; m_functor(); return *this; }
+
+        bool operator==(const T& rhs) const { return m_value == rhs; }
+        bool operator!=(const T& rhs) const { return m_value != rhs; }
+        bool operator>(const T& rhs) const { return m_value > rhs; }
+        bool operator>=(const T& rhs) const { return m_value >= rhs; }
+        bool operator<(const T& rhs) const { return m_value < rhs; }
+        bool operator<=(const T& rhs) const { return m_value <= rhs; }
+
+        friend std::ostream &operator<<(std::ostream &os, const robust::reference<T> &ref) {
+            return os << ref.m_value;
+        }
+
+    private:
+        T& m_value;
+        robust::functor &m_functor;
+    };
+
+} // namespace robust
+
+
+#endif /* BOOST_ROBUST_FUNCTOR_HPP */
