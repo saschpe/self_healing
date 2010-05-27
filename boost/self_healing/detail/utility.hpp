3867ef5c7b01b138694b0954a6561f6f1244487b
Sascha Peilicke <sasch.pe@gmx.de>
2010-05-27 22:41:02 +0200

    Added detail/utility.hpp for misc stuff. Currently contains only the templated 'to_string' function.

---
+++ boost/self_healing/detail/utility.hpp
@@ -0,0 +1,43 @@
+/*! \file
+* \brief Various utility functions.
+*
+* This file contains the function to_string().
+*
+* (C) Copyright Sascha Peilicke 2010.
+*
+* Distributed under the Boost Software License, Version 1.0. (See
+* accompanying file LICENSE_1_0.txt or copy at
+* http://www.boost.org/LICENSE_1_0.txt)
+*
+* 5 May 2010 - Initial Revision (Sascha Peilicke)
+*/
+
+#ifndef BOOST_SELF_HEALING_UTILITY_HPP
+#define BOOST_SELF_HEALING_UTILITY_HPP
+
+// FIXES for broken compilers
+#include <boost/config.hpp>
+
+#include <sstream>
+
+
+/// The namespace self_healing contains fault-tolerant data structures and utility classes.
+namespace boost { namespace self_healing {
+
+    /*! Converts any type into a std::string.
+    * \param value The value to convert.
+    * \return The value as a string.
+    */
+    template <class T>
+    inline std::string to_string(const T &value)
+    {
+        std::stringstream ss;
+        ss << value;
+        return ss.str();
+    }
+
+} } // namespace boost::self_healing
+
+
+#endif // BOOST_SELF_HEALING_UTILITY_HPP
+
