39d8b329d1cf57be1764b569116d92787705ac19
Sascha Peilicke <sasch.pe@gmx.de>
2010-05-27 23:05:52 +0200

    Expanded API documentation.

---
+++ libs/self_healing/doc/mainpage.h
@@ -0,0 +1,52 @@
+/*! \mainpage self_healing
+
+\section into_sec self_healing
+This is a cross-platform C++ library for self-healing and thus fault-rolerant
+data structures with a STL-compatible interface and released under the Boost
+Software License.
+
+
+\section dev_status_sec Development Status & Future Work
+Please have a look at the \ref todo "To-Do list" for open issues and ideas.
+You can also browse the public <A HREF="http://github.com/saschpe/self_healing">
+Git repository</A> to follow the development directly.
+
+
+\section  platforms_sec Supported Platforms
+self_healing is highly portable. It runs on any platform on which Boost is
+available: Linux, Windows, Mac OS 9/X, Solaris, etc.
+
+
+\section boost_sec Boost Libraries
+Boost is a collection of high-quality C++ libraries and works on almost any
+modern operating system, including UNIX and Windows variants.
+In fact threadpool is only a small extension to the portable Boost source
+libraries. For more information on this excellent library visit
+<A HREF="http://www.boost.org">Boost.org</A>.
+
+
+\section installation_sec Installing & Using threadpool
+Please have a look a the supplied README file for detailed installation
+instructions.
+
+
+\section license_sec Software License
+threadpool is released under the \ref license "Boost Software License".
+This license encourages both commercial and non-commercial use.
+
+
+\section acknowledgments_sec Acknowledgments
+This library was written by Sascha Peilicke <sasch.pe@gmx.de>.
+*/
+
+/*! \page license Software License
+\verbinclude LICENSE_1_0.txt
+*/
+
+/*! \page todo To-Do List
+\verbinclude TODO.txt
+*/
+ 
+/*! \page changelog Change Log
+\verbinclude ChangeLog.txt
+*/
