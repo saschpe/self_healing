f96041e40ad3476b0c69167490f47c59c0938ca7
Sascha Peilicke <sasch.pe@gmx.de>
2010-05-28 11:43:49 +0200

    Replaced template parameter name 'CS' with 'N' to resolve ambiguity with sizeof(vector_chunk<>).

--- boost/self_healing/detail/vector_chunk.hpp
+++ boost/self_healing/detail/vector_chunk.hpp
@@ -31,7 +31,7 @@
 /// The namespace self_healing contains fault-tolerant data structures and utility classes.
 namespace boost { namespace self_healing {
 
-    template <class T, std::size_t CS>
+    template <class T, std::size_t N>
     class vector;
 
     /*! \brief Vector element storage chunk.
@@ -40,12 +40,12 @@ namespace boost { namespace self_healing {
     * vector storage mechanism
     *
     * \param T The data type of the stored values.
-    * \param CS The size of the chunk.
+    * \param N The size of the chunk.
     * \throws std::invalid_argument Thrown if parent pointer is invalid.
     * \see checksummed_array
     */
-    template <class T, std::size_t CS = 64>
-    class vector_chunk : public child<vector<T, CS> >, public checksummed_array<T, CS>
+    template <class T, std::size_t N = 64>
+    class vector_chunk : public child<vector<T, N> >, public checksummed_array<T, N>
     {
     public:
         // type definitions
@@ -56,21 +56,21 @@ namespace boost { namespace self_healing {
         * \param parent The parent.
         * \param value An initial value that is set for all elements.
         */
-        explicit vector_chunk(vector<T, CS> *const parent, const_reference value = 0)
-            : child<vector<T, CS> >(parent), checksummed_array<value_type, CS>(value) {}
+        explicit vector_chunk(vector<value_type, N> *const parent, const_reference value = 0)
+            : child<vector<value_type, N> >(parent), checksummed_array<value_type, N>(value) {}
 
         /*! Validity check that tries to correct minor faults silently.
         * \param parent An optional pointer to the parent to check against.
         * \return true, if the internal structure and data is valid.
         * \see check_parent()
         */
-        bool is_valid(vector<T, CS> *const parent = NULL) const {
+        bool is_valid(vector<value_type, N> *const parent = NULL) const {
 #ifdef BOOST_SELF_HEALING_DEBUG
-            std::cout << "boost::self_healing::vector_chunk<T, CS>::is_valid()" << std::endl;
+            std::cout << "boost::self_healing::vector_chunk<T, N>::is_valid()" << std::endl;
 #endif
             // call the is_valid methods of all base classes.
-            return child<vector< T, CS> >::is_valid(parent) &&
-                   checksummed_array<value_type, CS>::is_valid();
+            return child<vector<value_type, N> >::is_valid(parent) &&
+                   checksummed_array<value_type, N>::is_valid();
         }
     };
 
