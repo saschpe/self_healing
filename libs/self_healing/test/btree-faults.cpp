8602fdff7a7b39d5e46a72a6f06df6366fa9de41
Sascha Peilicke <sasch.pe@gmx.de>
2010-05-31 13:16:59 +0200

    Added initial unit-test for a B-tree class. Added those to CMakeLists.txt.

---
+++ libs/self_healing/test/btree-faults.cpp
@@ -0,0 +1,23 @@
+/* Test case for class btree.
+ *
+ * (C) Copyright Sascha Peilicke 2010.
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * 31 May 2010 - Initial Revision (Sascha Peilicke)
+ */
+
+#include <boost/self_healing/btree.hpp>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "testing class boost::self_healing::btree<> fault injection" << std::endl;
+
+    boost::self_healing::btree<char> t;
+
+    return 0;
+}
