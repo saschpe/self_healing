e83cf51f3b6df50de850625a04a4c9f53f44cde3
Sascha Peilicke <sasch.pe@gmx.de>
2010-06-07 18:37:32 +0200

    Implemented the various fault-injection methods.

--- libs/self_healing/test/utility.hpp
+++ libs/self_healing/test/utility.hpp
@@ -25,29 +25,65 @@
 /// The namespace utility contains utility functions to provoke errors.
 namespace utility {
 
-    template <class DS>
-    void flipped_bits(DS * const start, std::size_t size, std::size_t bit_count = 1)
+    static const boost::rand48 s_rng;
+
+    /*! Function to randomly flip bits in a given memory region.
+    * \param obj Pointer to an object of type T.
+    * \param bit_count Amount of bits to flip.
+    */
+    template <class T>
+    void flip_bits(T * const obj, std::size_t bit_count = 1)
     {
-        //TODO:
+        // build a random number generator
+        static const boost::uniform_int<> dist(0, sizeof(T) * 8 - 1);
+        static boost::variate_generator<boost::rand48 &, boost::uniform_int<> > dice(s_rng, dist);
+
+        for (std::size_t i = 0; i < bit_count; i++) {
+            // determine the bit to flip and bit-manipulate the containing byte
+            const std::size_t bit_to_flip = dice();
+            const char &byte_to_modify = obj + bit_to_flip / 8;
+
+            byte_to_modify ^= 1 << (bit_to_flip % 8);
+        }
     }
 
-    template <class DS>
-    void burst_error(DS * const start, std::size_t size, std::size_t burst_length)
+    /*! Function to randomly flip bits in a consecutive region (burst).
+    * \param Pointer to an object of type T.
+    * \param burst_length Length of burst error.
+    */
+    template <class T>
+    void burst_flip_bits(T * const obj, std::size_t burst_length = 5)
     {
-        //TODO:
+        if (burst_length == 0) {
+            return;
+        }
+        if (burst_length >= sizeof(T) * 8) {
+            // if burst range is bigger than the bit-size of T set it to a sensible maximum
+            burst_length = sizeof(T) * 8;
+        }
+
+        // build a random number generator
+        static const boost::uniform_int<> dist(0, sizeof(T) * 8 - 1 - burst_length);
+        static boost::variate_generator<boost::rand48 &, boost::uniform_int<> > dice(s_rng, dist);
+        const std::size_t start_pos = dice();
+
+        for (std::size_t i = 0; i < burst_length; i++) {
+            // determine containing byte and flip the current bit
+            const char &byte_to_modify = obj + (start_pos + i) / 8;
+
+            byte_to_modify ^= 1 << ((start_pos + i) % 8);
+        }
     }
 
     /*! Function to change the value of a variable randomly.
     * \param var The variable to change.
-    * \param var The variable to change.
     */
     template <class T>
-    void value_change(T * const var)
+    void change_value(T * const var)
     {
-        // build a nice random number generator
-        boost::mt19937 rng;
-        boost::uniform_int<> dist(std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
-        boost::variate_generator<boost::mt19937 &, boost::uniform_int<> > dice(rng, dist);
+        // build a number generator
+        static const boost::uniform_int<> dist(std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+        static boost::variate_generator<boost::rand48 &, boost::uniform_int<> > dice(s_rng, dist);
 
         // apply random number to target
         *var = dice();
