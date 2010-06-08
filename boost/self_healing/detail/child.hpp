927982a032ac7817fadc7062d56f775fb4555687
Sascha Peilicke <sasch.pe@gmx.de>
2010-06-08 15:01:51 +0200

    Internally use TMR to store/fix pointer to parent in class 'child'. Expanded check/fix function.

--- boost/self_healing/detail/child.hpp
+++ boost/self_healing/detail/child.hpp
@@ -44,25 +44,35 @@ namespace boost { namespace self_healing {
         * \param parent The parent.
         * \throws std::invalid_argument Thrown if parent pointer is invalid.
         */
-        explicit child(parent_pointer const parent = NULL)
-            : m_parent(parent) {}
+        explicit child(parent_pointer const parent = NULL) {
+            set_parent(parent);
+        }
 
         /*! Copy constructor.
         * \param other The other child instance to copy from.
         */
-        child(const child &other)
-            : m_parent(other) {}
+        child(const child &other) {
+            set_parent(other.parent());
+        }
 
         /*! Set a new parent.
         * \param parent Pointer to the new parent.
         * \throws std::invalid_argument Thrown if parent pointer is invalid.
         */
-        void set_parent(parent_pointer const parent) { check_parent(parent); }
+        void set_parent(parent_pointer const parent) {
+            if (!parent) {
+                std::invalid_argument e("parent argument is NULL");
+                boost::throw_exception(e);
+            };
+            m_parent1 = parent;
+            m_parent2 = parent;
+            m_parent3 = parent;
+        }
 
         /*! Accessor to get the chunk's parent.
         * \return Pointer to the parent.
         */
-        parent_pointer parent() const { return m_parent; }
+        parent_pointer parent() const { check_parent(); return m_parent1; }
 
         /*! Validity check that tries to correct minor faults silently.
         * \param parent An optional pointer to the parent to check against.
@@ -88,26 +98,40 @@ namespace boost { namespace self_healing {
         /*! Checks the internal parent pointer against a provided one.
         * \throws std::invalid_argument Thrown if parent pointer is invalid.
         */
-        void check_parent(parent_pointer const parent) const {
+        void check_parent(parent_pointer const parent = NULL) const {
 #ifdef BOOST_SELF_HEALING_DEBUG
             std::cout << "boost::self_healing::child<P>::check_parent(" << parent << ")" << std::endl;
 #endif
             if (parent) {
                 // If a valid parent pointer was given we simply check against
                 // it and fix the internal pointer if needed.
-                if (m_parent != parent) {
-                    const_cast<parent_pointer &>(m_parent) = parent;
-                }
+                if (m_parent1 != parent) { const_cast<parent_pointer &>(m_parent1) = parent; }
+                if (m_parent2 != parent) { const_cast<parent_pointer &>(m_parent2) = parent; }
+                if (m_parent3 != parent) { const_cast<parent_pointer &>(m_parent3) = parent; }
             } else {
-                // No pointer was given to check against, so we can do only limited checks
-                if (!m_parent) {
-                    std::runtime_error e("parent is NULL");
+                // check and repair size via TMR voting
+                const bool equal_13 = m_parent1 == m_parent3;
+                const bool equal_23 = m_parent2 == m_parent3;
+                const bool equal_12 = m_parent2 == m_parent2;
+
+                if (equal_12 && equal_13 && equal_23) {
+                    // all fine
+                } else if (equal_13) {
+                    const_cast<parent_pointer &>(m_parent2) = m_parent1; // fix m_parent2 as the others are equal
+                } else if (equal_23) {
+                    const_cast<parent_pointer &>(m_parent1) = m_parent2; // fix m_parent1 as the others are equal
+                } else if (equal_12) {
+                    const_cast<parent_pointer &>(m_parent3) = m_parent1; // fix m_parent3 as the others are equal
+                } else {
+                    std::runtime_error e("parent error");
                     boost::throw_exception(e);
                 }
             }
         }
 
-        parent_pointer m_parent;    //!< Pointer to the parent class instance.
+        parent_pointer m_parent1;   //!< Pointer to the parent class instance.
+        parent_pointer m_parent2;   //!< Pointer to the parent class instance.
+        parent_pointer m_parent3;   //!< Pointer to the parent class instance.
     };
 
 } } // namespace boost::self_healing
