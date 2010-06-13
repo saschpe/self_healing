5f9eaff3465af58e264c8d873b04633836ca2f00
Sascha Peilicke <sasch.pe@gmx.de>
2010-06-13 19:47:08 +0200

    Added 'using namespace ..' to simplify code and added a delete.

--- libs/self_healing/test/checksummed_array.cpp
+++ libs/self_healing/test/checksummed_array.cpp
@@ -15,70 +15,74 @@
 #include <cassert>
 #include <memory>
 
+using namespace boost::self_healing;
+using namespace std;
+
 static void empty_function() {}
 
 int main()
 {
-    std::cout << "testing class boost::self_healing::checksummed_array<>" << std::endl;
+    cout << "testing class checksummed_array<>" << endl;
 
-    boost::self_healing::checksummed_array<int, 16> a;
-    std::cout << "checksummed_array 1: " << a << std::endl;
+    checksummed_array<int, 16> a;
+    cout << "checksummed_array 1: " << a << endl;
 
     int i = 3;
-    boost::self_healing::checksummed_array<int, 16>::reference ar(i, empty_function);
-    std::cout << "testing checksummed_array<>::reference constructor for " << ar << std::endl;
-    boost::self_healing::checksummed_array<int, 16>::const_reference car(i);
-    std::cout << "testing checksummed_array<>::const_reference constructor for " << car << std::endl;
+    checksummed_array<int, 16>::reference ar(i, empty_function);
+    cout << "testing checksummed_array<>::reference constructor for " << ar << endl;
+    checksummed_array<int, 16>::const_reference car(i);
+    cout << "testing checksummed_array<>::const_reference constructor for " << car << endl;
 
     assert(a.at(0) == 0);
     assert(a.at(a.size() - 1) == 0);
 
-    std::cout << "testing checksummed_array<>::reference assignment" << std::endl;
+    cout << "testing checksummed_array<>::reference assignment" << endl;
     a.front() = 3;
     a.back() = 4;
     a[2] = 4;
     a.at(3) = 5;
     a.at(13) = a.at(12);
 
-    std::cout << "testing checksummed_array<>::reference comparison" << std::endl;
+    cout << "testing checksummed_array<>::reference comparison" << endl;
     assert(a.front() == 3);
     assert(a.front() != 5);
     assert(a.front() >= 2);
-    std::cout << (a.front() <= 2) << std::endl;
-    std::cout << (a.front() > 3) << std::endl;
-    std::cout << (a.front() < 3) << std::endl;
+    cout << (a.front() <= 2) << endl;
+    cout << (a.front() > 3) << endl;
+    cout << (a.front() < 3) << endl;
 
-    std::cout << "testing checksummed_array<>::reference arithmetic operations" << std::endl;
-    std::cout << a.front() + 2 << std::endl;
-    std::cout << a.front() - 2 << std::endl;
-    std::cout << a.front() * 2 << std::endl;
-    std::cout << a.front() / 2 << std::endl;
-    std::cout << a.front() % 2 << std::endl;
+    cout << "testing checksummed_array<>::reference arithmetic operations" << endl;
+    cout << a.front() + 2 << endl;
+    cout << a.front() - 2 << endl;
+    cout << a.front() * 2 << endl;
+    cout << a.front() / 2 << endl;
+    cout << a.front() % 2 << endl;
 
-    std::cout << "testing checksummed_array<>::reference arithmetic assignment operations" << std::endl;
-    std::cout << a[0];
-    std::cout << "a1[6] += 1: " << (a[6] += 1) << std::endl;
-    std::cout << (a[7] -= 1) << std::endl;
-    std::cout << (a[7] *= 1) << std::endl;
-    std::cout << (a[7] /= 1) << std::endl;
-    std::cout << (a[7] %= 1) << std::endl;
+    cout << "testing checksummed_array<>::reference arithmetic assignment operations" << endl;
+    cout << a[0];
+    cout << "a1[6] += 1: " << (a[6] += 1) << endl;
+    cout << (a[7] -= 1) << endl;
+    cout << (a[7] *= 1) << endl;
+    cout << (a[7] /= 1) << endl;
+    cout << (a[7] %= 1) << endl;
 
-    std::cout << "testing checksummed_array<>::reference increment and decrement operations" << std::endl;
+    cout << "testing checksummed_array<>::reference increment and decrement operations" << endl;
     a[8]++;
     ++a[9];
     a[10]--;
     --a[11];
 
-    std::cout << "checksummed_array 1:  ";
-    std::copy(a.begin(), a.end(), std::ostream_iterator<int>(std::cout, ","));
-    std::cout << std::endl;
+    cout << "checksummed_array 1:  ";
+    copy(a.begin(), a.end(), ostream_iterator<int>(cout, ","));
+    cout << endl;
 
-    boost::self_healing::checksummed_array<int, 8> *pa = new boost::self_healing::checksummed_array<int, 8>(3);
-    std::cout << "pointer to checksummed_array: " << *pa << std::endl;
+    checksummed_array<int, 8> *pa = new checksummed_array<int, 8>(3);
+    cout << "pointer to checksummed_array: " << *pa << endl;
+    delete pa;
 
-    std::auto_ptr<boost::self_healing::checksummed_array<int, 8> > apa(pa);
-    std::cout << "auto_ptr to checksummed_array size: " << apa->size() << std::endl;
-    std::cout << "auto_ptr to checksummed_array: " << *apa << std::endl;
+    auto_ptr<checksummed_array<int, 8> > apa(pa);
+    cout << "auto_ptr to checksummed_array size: " << apa->size() << endl;
+    cout << "auto_ptr to checksummed_array: " << *apa << endl;
 
     return 0;
 }
