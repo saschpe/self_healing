b5acbf26b7b8a622b7e3a37d68bdb62e40c958dd
Sascha Peilicke <sasch.pe@gmx.de>
2010-07-20 21:14:42 +0200

    Added some minor test.

--- libs/self_healing/test/vector.cpp
+++ libs/self_healing/test/vector.cpp
@@ -13,22 +13,28 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "testing class boost::self_healing::vector<>" << std::endl;
+    cout << "testing class boost::self_healing::vector<>" << endl;
 
     int arr[4] = {1, 2, 3, 4};
 
-    std::cout << "initialize a vector from an array" << std::endl;
+    cout << "initialize a vector from an array" << endl;
     boost::self_healing::vector<int> numbers(arr, arr + 4);
 
-    std::cout << "insert more numbers into the vector" << std::endl;
+    cout << "insert more numbers into the vector" << endl;
     numbers.push_back(5);
     numbers.push_back(6);
     numbers.push_back(7);
     numbers.push_back(8);
-    std::cout << "the vector currently holds {1, 2, 3, 4, 5, 6, 7, 8}" << std::endl;
-    std::cout << "numbers: " << numbers << std::endl;
+    cout << "the vector currently holds {1, 2, 3, 4, 5, 6, 7, 8}" << endl;
+    cout << "numbers: " << numbers << endl;
+
+    cout << "getting first element via safe_ref" << endl;
+    int &first = numbers.front();
+
 
     return 0;
 }
