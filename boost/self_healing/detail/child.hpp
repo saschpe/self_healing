395cb355c6a4a161755e9dac854835f43f6cbcf1
Sascha Peilicke <sasch.pe@gmx.de>
2010-07-20 17:33:03 +0200

    The child class actually was never meant to implement TMR. It should simply provide a 'parent' member and convenience methods to other classes. The is_valid() method needs a parent parameter to compare with.

--- boost/self_healing/detail/child.hpp
+++ boost/self_healing/detail/child.hpp
@@ -26,7 +26,8 @@
 /// The namespace self_healing contains fault-tolerant data structures and utility classes.
 namespace boost { namespace self_healing {
 
-    /*! A class to express parent-child relationship.
+    /*! A class to express parent-child relationship. This class is not
+    * inherently fault-tolerant, but meant to be part of other classes.
     * \param Parent The type of the parent data structure.
     * \see array
     */
@@ -43,7 +44,7 @@ namespace boost { namespace self_healing {
         * \throws std::invalid_argument Thrown if parent pointer is invalid.
         */
         explicit child(parent_pointer const parent = 0)
-            : parent1(parent), parent2(parent), parent3(parent) {}
+            : p(parent) {}
 
         /*! Set a new parent.
         * \param parent Pointer to the new parent.
@@ -54,89 +55,33 @@ namespace boost { namespace self_healing {
                 std::invalid_argument e("parent argument is null");
                 boost::throw_exception(e);
             };
-            parent1 = parent;
-            parent2 = parent;
-            parent3 = parent;
+            p = parent;
         }
 
         /*! Accessor to get the chunk's parent.
         * \return Pointer to the parent.
         */
-        parent_pointer parent() const { check_parent(); return parent1; }
+        parent_pointer parent() const { return p; }
 
-        /*! Validity check that tries to correct minor faults silently.
+        /*! Validity check that needs a parent to compare with.
         * \param parent Optional pointer to the parent to check against.
         * \return true, if the internal structure and data is valid.
-        * \see check_parent()
         */
         bool is_valid(parent_pointer const parent = 0) const {
 #ifdef BOOST_SELF_HEALING_DEBUG
             std::cout << "boost::self_healing::child<Parent>::is_valid()" << std::endl;
 #endif
-            try {
-                check_parent(parent);
-                return true;
-            } catch (const std::runtime_error &e) {
-#ifdef BOOST_SELF_HEALING_DEBUG
-                std::cout << "boost::self_healing::child<Parent>::is_valid() caught runtime error: " << e.what() << std::endl;
-#endif
-                return false;
-            };
-        }
-
-    private:
-        /*! Checks the internal parent pointer against a provided one.
-        * \throws std::invalid_argument Thrown if parent pointer is invalid.
-        */
-        void check_parent(parent_pointer const parent = 0) const {
-#ifdef BOOST_SELF_HEALING_DEBUG
-            std::cout << "boost::self_healing::child<Parent>::check_parent(" << parent << ")" << std::endl;
-#endif
             if (parent) {
                 // If a valid parent pointer was given we simply check against
                 // it and fix the internal pointer if needed.
-                if (parent1 != parent) { const_cast<parent_pointer &>(parent1) = parent; }
-                if (parent2 != parent) { const_cast<parent_pointer &>(parent2) = parent; }
-                if (parent3 != parent) { const_cast<parent_pointer &>(parent3) = parent; }
+                if (parent != p) { const_cast<parent_pointer &>(p) = parent; }
             } else {
-                // check and repair size via TMR voting
-                const bool equal_13 = parent1 == parent3;
-                const bool equal_23 = parent2 == parent3;
-                const bool equal_12 = parent2 == parent2;
-
-                if (equal_12 && equal_13 && equal_23) {
-                    // all fine
-                } else if (equal_13) {
-#ifdef BOOST_SELF_HEALING_FIXING_CHECKS
-                    const_cast<parent_pointer &>(parent2) = parent1; // fix parent2 as the others are equal
-#else
-                    std::runtime_error e("fixable parent error");
-                    boost::throw_exception(e);
-#endif
-                } else if (equal_23) {
-#ifdef BOOST_SELF_HEALING_FIXING_CHECKS
-                    const_cast<parent_pointer &>(parent1) = parent2; // fix parent1 as the others are equal
-#else
-                    std::runtime_error e("fixable parent error");
-                    boost::throw_exception(e);
-#endif
-                } else if (equal_12) {
-#ifdef BOOST_SELF_HEALING_FIXING_CHECKS
-                    const_cast<parent_pointer &>(parent3) = parent1; // fix parent3 as the others are equal
-#else
-                    std::runtime_error e("fixable parent error");
-                    boost::throw_exception(e);
-#endif
-                } else {
-                    std::runtime_error e("parent error");
-                    boost::throw_exception(e);
-                }
+                return p != 0;
             }
         }
 
-        parent_pointer parent1; //!< Pointer to the parent class instance.
-        parent_pointer parent2; //!< Pointer to the parent class instance.
-        parent_pointer parent3; //!< Pointer to the parent class instance.
+    private:
+        parent_pointer p; //!< Pointer to the parent class instance.
     };
 
 } } // namespace boost::self_healing
