b81728bef0064ef7e3465340e3c0967a35158c0f
Sascha Peilicke <sasch.pe@gmx.de>
2010-08-15 22:32:18 +0200

    Added a more elaborate statistical test.

--- libs/self_healing/test/vector-faults.cpp
+++ libs/self_healing/test/vector-faults.cpp
@@ -9,31 +9,70 @@
  * 28 April 2010 - Initial Revision (Sascha Peilicke)
  */
 
+#include "utility.hpp"
+
 #include <boost/self_healing/vector.hpp>
 
 #include <iostream>
 
-#include "utility.hpp"
-
+using namespace boost;
 using namespace std;
 
 int main()
 {
     cout << "testing class boost::self_healing::vector<> fault injection" << endl;
 
-    boost::self_healing::vector<short> v;
-
-
-    cout << "statistical test ...\n" << endl;
-    size_t still_valid = 0;
-    for (size_t i = 0; i < 1000; i++) {
-        boost::self_healing::vector<int> tmp(23);
-        utility::flip_bits(&tmp, sizeof(tmp), 1);
-        if (tmp.is_valid()) {
-            still_valid++;
+    // bit flip tests
+    for (size_t bit_flips = 1; bit_flips <= 4; bit_flips++) {
+        size_t still_valid = 0;
+        for (size_t i = 0; i < 10000; i++) {
+            self_healing::vector<int> tmp(100);
+            utility::flip_bits(&tmp, sizeof(tmp), bit_flips);
+            try {
+                if (tmp.is_valid()) { still_valid++; }
+            } catch (...) {}
         }
+        cout << "vector<int>(100): " << still_valid << " out of 10000 are still valid for "
+             << bit_flips << " bit flip(s)." << endl;
     }
-    cout << still_valid << " out of 1000 vector<int> are still valid" << endl;
+    for (size_t bit_flips = 1; bit_flips <= 4; bit_flips++) {
+        size_t still_valid = 0;
+        for (size_t i = 0; i < 10000; i++) {
+            self_healing::vector<int> tmp(1000);
+            utility::flip_bits(&tmp, sizeof(tmp), bit_flips);
+            try {
+                if (tmp.is_valid()) { still_valid++; }
+            } catch (...) {}
+        }
+        cout << "vector<int>(1000): " << still_valid << " out of 10000 are still valid for "
+             << bit_flips << " bit flip(s)." << endl;
+    }
+
+    // burst tests
+    for (size_t burst_length = 4; burst_length <= 16; burst_length *= 2) {
+        size_t still_valid = 0;
+        for (size_t i = 0; i < 1000; i++) {
+            self_healing::vector<int> tmp(100);
+            utility::burst_flip_bits(&tmp, sizeof(tmp), burst_length);
+            try {
+                if (tmp.is_valid()) { still_valid++; }
+            } catch (...) {}
+        }
+        cout << "vector<int>(100): " << still_valid << " out of 10000 are still valid for burst of length "
+             << burst_length << endl;
+    }
+    for (size_t burst_length = 4; burst_length <= 16; burst_length *= 2) {
+        size_t still_valid = 0;
+        for (size_t i = 0; i < 1000; i++) {
+            self_healing::vector<int> tmp(1000);
+            utility::burst_flip_bits(&tmp, sizeof(tmp), burst_length);
+            try {
+                if (tmp.is_valid()) { still_valid++; }
+            } catch (...) {}
+        }
+        cout << "vector<int>(1000): " << still_valid << " out of 10000 are still valid for burst of length "
+             << burst_length << endl;
+    }
 
     return 0;
 }
